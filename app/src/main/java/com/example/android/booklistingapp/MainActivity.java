7b6a7f2844c1c76a7591b431b5743f86b77d54fb
doppelledev <doppelledev@gmail.com>
2018-08-13 12:01:46 +0100

    Add search FragmentPagerAdapter and SearchFragment

--- app/src/main/java/com/example/android/booklistingapp/MainActivity.java
+++ app/src/main/java/com/example/android/booklistingapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.booklistingapp;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acivity_main);
 
+        ViewPager pager = findViewById(R.id.pager);
+        MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(this, getSupportFragmentManager());
+        pager.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/android/booklistingapp/MyFragmentPagerAdapter.java
@@ -0,0 +1,29 @@
+package com.example.android.booklistingapp;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private Context context;
+
+    public MyFragmentPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            default:
+                return new SearchFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
