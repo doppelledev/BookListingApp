bfc78b9897be5ed5eb02395be3b3adc076bac26a
doppelledev <doppelledev@gmail.com>
2018-08-13 22:56:55 +0100

    Style TabLayout

--- app/src/main/java/com/example/android/booklistingapp/MainActivity.java
+++ app/src/main/java/com/example/android/booklistingapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.booklistingapp;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -14,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
         ViewPager pager = findViewById(R.id.pager);
         MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(this, getSupportFragmentManager());
         pager.setAdapter(adapter);
+        TabLayout tab = findViewById(R.id.pager_tab);
+        tab.setupWithViewPager(pager);
     }
 }
--- app/src/main/java/com/example/android/booklistingapp/MyFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/booklistingapp/MyFragmentPagerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.booklistingapp;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -9,7 +10,7 @@ public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
 
     private Context context;
 
-    public MyFragmentPagerAdapter(Context context, FragmentManager fm) {
+    MyFragmentPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
         this.context = context;
     }
@@ -17,13 +18,28 @@ public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
     @Override
     public Fragment getItem(int position) {
         switch (position) {
-            default:
+            case 0:
                 return new SearchFragment();
+            default:
+                return new LibraryFragment();
         }
     }
 
     @Override
     public int getCount() {
         return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return context.getString(R.string.search_frag);
+            case 1:
+                return context.getString(R.string.library_frag);
+            default:
+                return null;
+        }
     }
 }
